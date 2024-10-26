3f8d992a55babbb8a52b78d76073db7f436bb819
Mondmarmelade <91614367+Mondmarmelade@users.noreply.github.com>
2024-10-27 00:27:58 +0200

    feat: :sparkles: Adds EmailList
    EmailList to render all emails from Inbox on IMAP-Server

---
+++ src/components/EmailListItem.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import "../styles/EmailListItem.css";
+
+const EmailListItem = ({ element }) => {
+  console.log(element);
+
+  return (
+    <div className="email-list-item" style={{ width: "100%" }}>
+      <img
+        src={`https://logo.clearbit.com/${
+          element.envelope.from[0].address.split("@")[1]
+        }`}
+      />
+      {/* <input type="checkbox"></input> */}
+      <p className="address">
+        {element.envelope.from[0].name !== ""
+          ? element.envelope.from[0].name
+          : element.envelope.from[0].address}
+      </p>
+      <p>{element.envelope.subject}</p>
+    </div>
+  );
+};
+
+export default EmailListItem;
